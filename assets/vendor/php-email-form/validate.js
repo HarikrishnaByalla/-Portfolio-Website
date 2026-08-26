cf02e8355a294ed7049981fe132aa384ad922e23
vilcartdm-wq <vilcartdm@gmail.com>
2026-08-26 16:54:22 +0530

    Show contact form server errors

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -55,13 +55,13 @@
       body: formData,
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+    .then(response => response.text().then(data => {
+      if (response.ok) {
+        return data;
       }
-    })
+
+      throw new Error(data ? data : `${response.status} ${response.statusText} ${response.url}`);
+    }))
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
       if (data.trim() == 'OK') {
